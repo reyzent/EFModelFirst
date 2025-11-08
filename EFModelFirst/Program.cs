8cccf2d11fa6985f048f398cc15f87674342a075
Kirill Sevastyanov <reyzent@vk.com>
2025-11-09 01:11:31 +1100

    First commit: EF Model First approach

---
+++ EFModelFirst/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFModelFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(UserContainer db = new UserContainer())
+            {
+                // Добавление элементов
+                db.Users.Add(new User { Name = "Tom", Age = 45 });
+                db.Users.Add(new User { Name = "John", Age = 22 });
+                db.SaveChanges();
+                // Получение элементов
+                var users = db.Users;
+                foreach (User u in users)
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ EFModelFirst/User.edmx.sql
@@ -0,0 +1,57 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 11/07/2025 16:01:38
+-- Generated from EDMX file: C:\Users\reyzent\source\repos\EFModelFirstSolution\EFModelFirst\User.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [usersdb];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Users]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Users];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [Age] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
